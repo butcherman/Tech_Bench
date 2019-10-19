013a5b9e9e794f3a4692da4fd08d643ec66116a6
Butcherman <ronbutcher@att.net>
2019-10-18 18:43:55 -0700

    Re-built Template with new UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,53 +3,111 @@
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{csrf_token()}}">
-
     <title>{{config('app.name', 'Tech Bench')}}</title>
-    <noscript><meta http-equiv="refresh" content="0; url={{route('noscript')}}" /></noscript>
-    <script>
-        window.techBench = {
-            'maxUpload': '{{config('filesystems.paths.max_size')}}',
-            'csrfToken': '{{csrf_token()}}'
-        };
-    </script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}"></script>
 </head>
-
 <body>
-    <div id="app">
-        <navbar 
-            brand="{{config('app.name', 'Tech Bench')}}"
-            dashboard_route="{{route('index')}}"
-            logout_route="{{route('logout')}}"
-            help_route="{{route('about')}}"
-            links_route="{{route('links.index')}}"
-            account_route="{{route('account')}}"
-            cust_route="{{route('customer.index')}}"
-            tech_tip_route="{{route('tips.index')}}"
-        @if(Auth::user()->hasAnyRole(['installer', 'admin']))    
-            admin_route="{{route('admin.index')}}"  
-        @endif
-            sys_list="{{json_encode($navArray)}}"
-        ></navbar>
-        <div class="content-wrapper">
-            <div class="container-fluid">
-                @yield('breadcrumbs')
-                @yield('content')
+    <div class="container-scroller">
+        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo mr-5" href="{{route('dashboard')}}"><img src="{{config('app.logo')}}" class="mr-2" alt="Tech Bench"/></a>
+                <a class="navbar-brand brand-logo-mini" href="{{route('dashboard')}}"><img src="{{config('app.logo')}}" alt="Tech Bench"/></a>
+            </div>    
+            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+
+                <ul class="navbar-nav mr-lg-2">
+                    <li class="nav-item nav-search d-none d-lg-block">
+                        <h2>{{config('app.name', 'Tech Bench')}}</h2>
+                    </li>
+                </ul>
+                <ul class="navbar-nav navbar-nav-right">
+<!--               Notifications
+                   <li class="nav-item dropdown">
+                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+                            <i class="ti-bell mx-0"></i>
+                            <span class="count"></span>
+                        </a>
+                    </li>
+-->
+                    <li class="nav-item dropdown mr-1">
+                        <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="{{route('about')}}">
+                            <i class="ti-help-alt mx-0"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item nav-profile dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="profileDropdown" onclick="return expandProfile();">
+                            <img src="img/profile_pic.png" alt="profile"/>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown" id="profileDropdownDiv">
+                            <a href="{{route('account')}}" class="dropdown-item">
+                                <i class="ti-settings text-primary"></i>
+                                Settings
+                            </a>
+                            <a href="{{route('logout')}}" class="dropdown-item">
+                                <i class="ti-power-off text-primary"></i>
+                                Logout
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" onclick="expandNav()">
+                    <span class="ti-view-list"></span>
+                </button>
+            </div>
+        </nav>
+    <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('dashboard')}}">
+                            <i class="ti-home menu-icon"></i>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('admin.index')}}">
+                            <i class="ti-pencil-alt menu-icon"></i>
+                            <span class="menu-title">Administration</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('links.index')}}">
+                            <i class="ti-link menu-icon"></i>
+                            <span class="menu-title">File Links</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('customer.index')}}">
+                            <i class="ti-user menu-icon"></i>
+                            <span class="menu-title">Customers</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('tips.index')}}">
+                            <i class="ti-hummer menu-icon"></i>
+                            <span class="menu-title">Tech Tips</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright &copy; Tech Bench 2016-2020 - All rights reserved.</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">@version('full')</span>
+                    </div>
+                </footer>
             </div>
         </div>
-        <footer class="sticky-footer">
-            <div class="container">
-                <div class="text-center">
-                    <small>Copyright &copy; Tech Bench 2016-2019 - Version - @version('version')</small>
-                </div>
-            </div>
-        </footer>
-    </div>
+  </div>
 </body>
 </html>
+
