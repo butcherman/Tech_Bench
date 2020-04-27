d8e18e242251fa3fff8a578c86513f5186f92643
Butcherman <ronbutcher@att.net>
2020-04-27 15:56:03 -0700

    Updated update file

--- app/Console/Commands/updateRun.php
+++ app/Console/Commands/updateRun.php
@@ -9,20 +9,16 @@ use Illuminate\Support\Facades\Storage;
 
 class updateRun extends Command
 {
-    protected $signature = 'tb-update:run';
+    protected $signature   = 'tb-update:run';
     protected $description = 'Update the Tech Bench to a newer version';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     public function handle()
     {
         $this->line('');
         //  Select which update file to use
         $updateFile = $this->checkForUpdate();
-        if($updateFile) {
+        if($updateFile)
+        {
             //  Open up the file and verify it is at least the same version as the current setup
             $valid = $this->openUpdate($updateFile);
 
@@ -42,7 +38,7 @@ class updateRun extends Command
     }
 
     //  Check to see if any backups are in the backup folder
-    private function checkForUpdate()
+    protected function checkForUpdate()
     {
         $updateList = [];
         $updates    = Storage::disk('staging')->files('updates');
@@ -96,7 +92,7 @@ class updateRun extends Command
     }
 
     //  Unzip the update and verify it is a newer or the same version
-    private function openUpdate($file)
+    protected function openUpdate($file)
     {
         $fileParts = pathinfo($file);
         $folder = $fileParts['filename'];
@@ -151,7 +147,7 @@ class updateRun extends Command
     }
 
     //  copy the update files to the proper folders
-    private function copyFiles($updateFile)
+    protected function copyFiles($updateFile)
     {
         $fileParts = pathinfo($updateFile);
         $folder = $fileParts['filename'];
@@ -161,18 +157,22 @@ class updateRun extends Command
             DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR;
 
         // Copy files
-		// TODO: Copy composer and package files
-        File::copyDirectory($updateFile.'app', base_path().DIRECTORY_SEPARATOR.'app');
+        File::copyDirectory($updateFile.'app',       base_path().DIRECTORY_SEPARATOR.'app');
         File::copyDirectory($updateFile.'bootstrap', base_path().DIRECTORY_SEPARATOR.'bootstrap');
-        File::copyDirectory($updateFile.'config', base_path().DIRECTORY_SEPARATOR.'config');
-        File::copyDirectory($updateFile.'database', base_path().DIRECTORY_SEPARATOR.'database');
+        File::copyDirectory($updateFile.'config',    base_path().DIRECTORY_SEPARATOR.'config');
+        File::copyDirectory($updateFile.'database',  base_path().DIRECTORY_SEPARATOR.'database');
+        File::copyDirectory($updateFile.'public',    base_path().DIRECTORY_SEPARATOR.'public');
         File::copyDirectory($updateFile.'resources', base_path().DIRECTORY_SEPARATOR.'resources');
-        File::copyDirectory($updateFile.'routes', base_path().DIRECTORY_SEPARATOR.'routes');
+        File::copyDirectory($updateFile.'routes',    base_path().DIRECTORY_SEPARATOR.'routes');
+        File::copy($updateFile.'composer.json',      base_path().DIRECTORY_SEPARATOR.'composer.json');
+        File::copy($updateFile.'composer.lock',      base_path().DIRECTORY_SEPARATOR.'composer.lock');
+        File::copy($updateFile.'package.json',       base_path().DIRECTORY_SEPARATOR.'package.json');
+        File::copy($updateFile.'package-lock.json',  base_path().DIRECTORY_SEPARATOR.'package-lock.json');
 
         //  Run Composer Updates
         exec('cd '.base_path().' && composer install --no-dev --no-interaction --optimize-autoloader --no-ansi > /dev/null 2>&1');
         $this->call('ziggy:generate');
-        //  Run NPM
+        //  Run NPM Updates
         exec('cd '.base_path().' && npm install --only=production > /dev/null 2>&1');
         exec('cd '.base_path().' && npm run production > /dev/null 2>&1');
 
