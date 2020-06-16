8939621d7f44a5c15e14f9354b5f9f130b7a1e40
Butcherman <ronbutcher@att.net>
2020-06-15 19:42:41 -0700

    Update SystemTypesFactory.php

--- database/factories/SystemTypesFactory.php
+++ database/factories/SystemTypesFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(SystemTypes::class, function(Faker $faker) {
     return [
-        'cat_id'          => factory(App\SystemCategories::class),
+        'cat_id'          => factory(App\SystemCategories::class)->cat_id,
         'name'            => $name = $faker->unique()->name(),
         'folder_location' => $name
     ];
