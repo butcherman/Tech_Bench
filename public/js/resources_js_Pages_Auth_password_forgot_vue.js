801cb12cdd2866b4020f1c5c394b6f3a0471622c
Butcherman <ronbutcher@att.net>
2022-02-03 20:05:00 -0800

    Fixed login form styling

--- public/js/resources_js_Pages_Auth_password_forgot_vue.js
+++ public/js/resources_js_Pages_Auth_password_forgot_vue.js
@@ -269,7 +269,7 @@ var render = function() {
             attrs: { id: "auth-layout-sub-container" }
           },
           [
-            _c("div", { staticClass: "col col-md-3 col-10" }, [
+            _c("div", { staticClass: "col col-lg-3 col-10" }, [
               _c("img", {
                 attrs: {
                   src: _vm.$page.props.app.logo,
@@ -281,7 +281,7 @@ var render = function() {
             _vm._v(" "),
             _c(
               "div",
-              { staticClass: "col col-md-3 col-10" },
+              { staticClass: "col col-lg-3 col-10" },
               [_vm._t("default")],
               2
             )
