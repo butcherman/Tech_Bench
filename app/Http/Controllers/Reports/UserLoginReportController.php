f0d6193fdab2eaef7f68e899ef641205daf525ff
Butcherman <ronbutcher@att.net>
2022-05-07 17:14:30 -0700

    Added User Permissions Report

--- app/Http/Controllers/Reports/UserLoginReportController.php
+++ app/Http/Controllers/Reports/UserLoginReportController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Reports;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Reports\UserLoginReportRequest;
 use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class UserLoginReportController extends Controller
@@ -20,45 +18,12 @@ class UserLoginReportController extends Controller
             'user_list' => User::all(),
         ]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
     /**
      * Display the specified resource
      */
     public function show($id)
     {
         return redirect(route('reports.user-login-report.index'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -96,16 +61,5 @@ class UserLoginReportController extends Controller
         return Inertia::render('Reports/UserLogin/Report', [
             'data' => $userList,
         ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
