ca86de9a297bb99b813ea483e83ce756befbe69d
Butcherman <ronbutcher@att.net>
2023-04-25 20:24:20 -0700

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode} ) => {
+
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    const wsHost = process.env.VITE_WS_HOST !== undefined ? process.env.VITE_WS_HOST : 'localhost';
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.ts',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
-    server: {
-        https: false,
-        host: '0.0.0.0',
-        hmr: {
-            protocol: 'ws',
-            host: 'localhost',
+            }),
+        ],
+        server: {
+            https: false,
+            host: '0.0.0.0',
+            hmr: {
+                protocol: 'ws',
+                host: wsHost
+            }
         }
     }
 });
