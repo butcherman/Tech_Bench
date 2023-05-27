0d941a51a2843f23babd93c780f52ced480599ab
Butcherman <ronbutcher@att.net>
2023-05-27 14:45:07 -0700

    Removed Horizon Routes from log debug routes

--- app/Http/Middleware/LogDebugVisits.php
+++ app/Http/Middleware/LogDebugVisits.php
@@ -18,27 +18,31 @@ class LogDebugVisits
     protected $redact = ['password', 'password_confirmation', 'current_password'];
 
     /**
-\     * When Debug Logging is one, log every page visit and all $request data
+\     * When Debug Logging is one, log every page visit and all $request data - Except Horizon Routes
      */
     public function handle(Request $request, Closure $next)
     {
         $user = isset(Auth::user()->user_id) ? Auth::user()->full_name : \Request::ip();
         $requestData = $request->request->all();
 
-        Log::debug('Route '.Route::currentRouteName().' visited by '.$user);
+        $routeArray = explode('.', Route::currentRouteName());
+        if($routeArray[0] !== 'horizon')
+        {
+            Log::debug('Route '.Route::currentRouteName().' visited by '.$user);
 
-        if (! empty($requestData)) {
-            foreach ($this->redact as $i) {
-                if (Arr::exists($requestData, $i)) {
-                    $requestData[$i] = '[REDACTED]';
+            if (! empty($requestData)) {
+                foreach ($this->redact as $i) {
+                    if (Arr::exists($requestData, $i)) {
+                        $requestData[$i] = '[REDACTED]';
+                    }
                 }
-            }
-            foreach ($this->ignore as $i) {
-                if (Arr::exists($requestData, $i)) {
-                    unset($requestData[$i]);
+                foreach ($this->ignore as $i) {
+                    if (Arr::exists($requestData, $i)) {
+                        unset($requestData[$i]);
+                    }
                 }
+                Log::debug('Submitted Data ', $requestData);
             }
-            Log::debug('Submitted Data ', $requestData);
         }
 
         return $next($request);
