c74894f91c2ab901afc7cf36a5b5a1465070735b
Butcherman <ronbutcher@att.net>
2023-05-29 21:08:40 -0700

    Added shake animation to login form.

--- resources/js/Pages/Auth/UserLogin.vue
+++ resources/js/Pages/Auth/UserLogin.vue
@@ -3,6 +3,8 @@
     <AuthLayout>
         <VueForm
             ref="loginForm"
+            id="loginForm"
+            :class="{ 'apply-shake': shake }"
             :validation-schema="validationSchema"
             submit-text="Login"
             @submit="onSubmit"
@@ -55,7 +57,7 @@ import AuthLayout from "@/Layouts/authLayout.vue";
 import VueForm from "@/Components/Base/VueForm.vue";
 import TextInput from "@/Components/Base/Input/TextInput.vue";
 import CheckboxSwitch from "@/Components/Base/Input/CheckboxSwitch.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useForm } from "@inertiajs/vue3";
 import { object, string } from "yup";
 
@@ -69,6 +71,13 @@ const validationSchema = object({
     password: string().required("You must enter a password"),
 });
 
+const shake = ref(false);
+onMounted(() =>
+    document
+        .getElementById("loginForm")
+        ?.addEventListener("animationend", () => (shake.value = false))
+);
+
 type loginFormType = {
     username: string;
     password: string;
@@ -78,6 +87,36 @@ const onSubmit = (form: loginFormType) => {
     const submittedData = useForm(form);
     submittedData.post(route("login.submit"), {
         onFinish: () => loginForm.value?.endSubmit(),
+        onError: () => (shake.value = true),
     });
 };
 </script>
+
+<style scoped>
+@keyframes shake {
+    10%,
+    90% {
+        transform: translate3d(-1px, 0, 0);
+    }
+
+    20%,
+    80% {
+        transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+        transform: translate3d(-4px, 0, 0);
+    }
+
+    40%,
+    60% {
+        transform: translate3d(4px, 0, 0);
+    }
+}
+
+.apply-shake {
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+}
+</style>
