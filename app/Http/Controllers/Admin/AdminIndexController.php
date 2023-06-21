c7c4d0e9242625a0c239edc7ab36f48c870339fc
Butcherman <ronbutcher@att.net>
2023-06-20 21:26:07 -0700

    Added Log Debug Visits

--- app/Http/Controllers/Admin/AdminIndexController.php
+++ app/Http/Controllers/Admin/AdminIndexController.php
@@ -6,6 +6,7 @@ use App\Actions\BuildAdminMenu;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class AdminIndexController extends Controller
