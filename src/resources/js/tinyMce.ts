e7c72b355e986141a03cf5d2ab41f485af54de60
butcherman <ronbutcherii@gmail.com>
2024-03-16 21:33:56 +0000

    Update Editor Component

---
+++ src/resources/js/tinyMce.ts
@@ -0,0 +1,35 @@
+/**
+ * Import TinyMCE Base Package
+ */
+import "tinymce";
+
+/**
+ * Required TinyMCE Components
+ */
+import "tinymce/icons/default/icons.min.js";
+import "tinymce/themes/silver/theme.min.js";
+import "tinymce/models/dom/model.min.js";
+
+/**
+ * TinyMCE Skin & Styling
+ */
+import "tinymce/skins/ui/oxide/skin.js";
+import "tinymce/skins/ui/oxide/content.js";
+import "tinymce/skins/ui/oxide/content.inline.js";
+import "tinymce/skins/content/default/content.js";
+import "tinymce/skins/ui/oxide/skin.shadowdom.js";
+
+/**
+ * TinyMCE Plugins
+ */
+
+/* Import plugins */
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/code";
+import "tinymce/plugins/image";
+import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/pagebreak";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/table";
