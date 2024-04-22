da5aec42b3cd7f6e8b0b583939aa90b2637ab7d9
butcherman <ronbutcherii@gmail.com>
2024-04-22 01:11:25 +0000

    Upgrade to Laravel 11

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <title inertia>{{ config('app.name') }}</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        @routes
+        @routes()
         @vite('resources/js/app.ts')
         @inertiaHead
     </head>
