75a26a57ee69d90d8354e0a76e15e9c399e971fb
butcherman <ronbutcherii@gmail.com>
2024-07-27 23:03:30 +0000

    Add Tech Tip Search Pages

---
+++ src/resources/js/Help/Pages/tech-tips.edit.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h5 class="text-center">Edit Tech Tip</h5>
+        <p class="text-center">
+            Use this form to to modify an existing Tech Tip.
+        </p>
+        <techTipForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import techTipForm from "../Forms/tech-tips.techTipForm.vue";
+</script>
---
+++ src/resources/js/Help/Pages/tech-tips.index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center">
+        <h5>Tech Tips</h5>
+        <p>
+            Tech Tips are Knowledge Base articles to help make your job easier.
+        </p>
+        <p>
+            Tips can range from official documentation, to tips and tricks that
+            you and others have run into while working on equipment.
+        </p>
+        <p>
+            Use the Search Bar to search through the Tech Tips and use the
+            Search Filters to narrow down the results.
+        </p>
+    </div>
+</template>
