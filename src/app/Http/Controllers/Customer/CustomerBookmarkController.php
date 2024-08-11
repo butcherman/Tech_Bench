4b2da4c250789a78ec1d830ec06732f5c1d911ae
butcherman <ronbutcherii@gmail.com>
2024-08-11 01:35:57 +0000

    Add Recent and Bookmarked Customers to Dashboard

---
+++ src/app/Http/Controllers/Customer/CustomerBookmarkController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Home\BookmarkRequest;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerBookmarkController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(BookmarkRequest $request, Customer $customer)
+    {
+        if ($request->value) {
+            $customer->Bookmarks()->attach($request->user());
+        } else {
+            $customer->Bookmarks()->detach($request->user());
+        }
+
+        return response(['success' => true]);
+    }
+}
