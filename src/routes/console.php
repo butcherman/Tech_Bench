e471ad881db45315722b286bba75e0cfd6e5effc
butcherman <ronbutcherii@gmail.com>
2024-09-17 19:19:48 -0700

    Add Check Azure Secret Job

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\Admin\CheckAzureCertificateJob;
 use App\Jobs\Admin\CheckCertificateJob;
 use App\Jobs\Maintenance\DailyCleanupJob;
 use App\Jobs\Maintenance\ImageFileCleanupJob;
@@ -13,6 +14,7 @@ Schedule::command('auth:clear-resets')->everyFifteenMinutes();
 Schedule::command('horizon:snapshot')->everyFifteenMinutes();
 Schedule::command('model:prune')->daily();
 Schedule::job(new CheckCertificateJob)->daily();
+Schedule::job(new CheckAzureCertificateJob)->daily();
 Schedule::job(new NightlyBackupJob)->dailyAt('03:00');
 Schedule::job(new DailyCleanupJob)->dailyAt('06:00');
 Schedule::job(new ImageFileCleanupJob)->monthly();
