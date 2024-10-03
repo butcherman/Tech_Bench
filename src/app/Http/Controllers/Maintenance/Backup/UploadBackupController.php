4d6efa8a19eefa9132636c56251a4fbbaada4fd3
butcherman <ronbutcherii@gmail.com>
2024-10-02 20:26:00 -0700

    Increase Testing

--- src/app/Http/Controllers/Maintenance/Backup/UploadBackupController.php
+++ src/app/Http/Controllers/Maintenance/Backup/UploadBackupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Maintenance\Backup;
 
 use App\Http\Controllers\Controller;
+use App\Models\AppSettings;
 use App\Traits\FileTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -16,6 +17,8 @@ class UploadBackupController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $this->authorize('viewAny', AppSettings::class);
+
         $this->setFileData('backups', 'tech-bench');
 
         if ($savedFile = $this->getChunk($request)) {
