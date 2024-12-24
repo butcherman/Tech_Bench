d0dab7168ce99455ddb26ccd1ce65aae9df15c3e
butcherman <ronbutcherii@gmail.com>
2024-12-23 18:37:13 -0800

    Add Change Password Form

---
+++ src/resources/js/Forms/User/UserPasswordForm.vue
@@ -0,0 +1,65 @@
+<template>
+    <VueForm
+        ref="userPasswordForm"
+        :initial-values="initValues"
+        :validation-schema="schema"
+        :submit-route="submitRoute"
+        submit-method="put"
+        submit-text="Update Password"
+        @success="onSuccess"
+    >
+        <PasswordInput
+            id="current-password"
+            name="current_password"
+            label="Current Password"
+            v-focus
+        />
+        <PasswordInput id="password" name="password" label="New Password" />
+        <PasswordInput
+            id="password-confirmed"
+            name="password_confirmation"
+            label="Confirm Password"
+        />
+    </VueForm>
+</template>
+
+<script setup lang="ts">
+import VueForm from "@/Forms/_Base/VueForm.vue";
+import PasswordInput from "../_Base/PasswordInput.vue";
+import { computed, ref } from "vue";
+import { object, string, ref as reference } from "yup";
+
+const emit = defineEmits(["success"]);
+const props = defineProps<{
+    init?: boolean;
+}>();
+
+const userPasswordForm = ref<InstanceType<typeof VueForm> | null>(null);
+
+const submitRoute = computed<string>(() =>
+    props.init ? route("init.step-4b.submit") : route("user-password.update")
+);
+
+const onSuccess = (): void => {
+    emit("success");
+    userPasswordForm.value?.resetForm();
+};
+
+/*
+|---------------------------------------------------------------------------
+| Validation
+|---------------------------------------------------------------------------
+*/
+const initValues = {
+    current_password: null,
+    password: null,
+    password_confirmation: null,
+};
+const schema = object({
+    current_password: string().required("Enter your current password"),
+    password: string().required(),
+    password_confirmation: string()
+        .required("You must confirm your password")
+        .oneOf([reference("password")], "Passwords must match"),
+});
+</script>
--- src/resources/js/Pages/User/Password.vue
+++ src/resources/js/Pages/User/Password.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-        <h4 class="text-center font-bold">Hello World</h4>
-        <v-card elevation="2" subtitle="This is a Title">
-            <p class="text-center">This is a new page</p>
-        </v-card>
+    <div class="grid gap-2 md:place-items-center">
+        <Card class="reasonable-width">
+            <UserPasswordForm />
+        </Card>
+
+        <Card class="reasonable-width">
+            <h5 class="text-center font-bold">Password Rules</h5>
+            <ul class="text-center">
+                <li v-for="rule in rules" class="border p-2">{{ rule }}</li>
+            </ul>
+        </Card>
     </div>
 </template>
 
 <script setup lang="ts">
 import AppLayout from "@/Layouts/App/AppLayout.vue";
-import { ref, reactive, onMounted } from "vue";
+import Card from "@/Components/_Base/Card.vue";
+import UserPasswordForm from "@/Forms/User/UserPasswordForm.vue";
 
-const props = defineProps<{}>();
+defineProps<{
+    rules: string[];
+}>();
 </script>
 
 <script lang="ts">
