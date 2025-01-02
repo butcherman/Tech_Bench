484a930aa63ed4fd9ec60959287957857e528eda
butcherman <ronbutcherii@gmail.com>
2025-01-01 16:46:54 -0800

    Add Cookie Consent

--- src/resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ src/resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,8 +1,13 @@
-<div class="alert alert-warning fixed-bottom m-0 mb-4 js-cookie-consent">
+<div
+    class="js-cookie-consent absolute inset-x-0 bottom-0 bg-orange-100 border-l-4 text-orange-700 p-4"
+    style="z-index: 10000"
+>
     <div class="text-center">
         {!! __('cookie-consent::texts.message') !!}
-        <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer btn btn-primary">
-            {{ __('cookie-consent::texts.agree')  }}
+        <button
+            class="js-cookie-consent-agree cookie-consent__agree cursor-pointer bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        >
+            {{ __('cookie-consent::texts.agree') }}
         </button>
     </div>
 </div>
