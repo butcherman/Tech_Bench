adda93aae97eff52c89bbb413e01941d1bd8c67e
butcherman <ronbutcherii@gmail.com>
2025-01-08 20:14:58 -0800

    Add Step Navigation

--- src/resources/js/Pages/Home/Typography.vue
+++ src/resources/js/Pages/Home/Typography.vue
@@ -9,6 +9,13 @@
             <AdvancedButtons id="advanced-buttons" />
         </div>
         <DataComponents id="data-tables" />
+        <Card class="my-3" title="Step Navigation">
+            <StepNavigation
+                id="step-navigation"
+                :step-list="stepList"
+                :current-step="1"
+            />
+        </Card>
         <div class="grid md:grid-cols-2 gap-3">
             <BasicForm id="basic-forms" />
             <AdvancedForm id="advanced-forms" />
@@ -26,6 +33,8 @@ import DataComponents from "@/Components/_typography/DataComponents.vue";
 import BasicForm from "@/Components/_typography/BasicForm.vue";
 import AdvancedForm from "@/Components/_typography/AdvancedForm.vue";
 import QuickJump from "@/Components/_Base/QuickJump.vue";
+import StepNavigation from "@/Components/_Base/StepNavigation.vue";
+import Card from "@/Components/_Base/Card.vue";
 
 const quickJumpList = [
     {
@@ -49,12 +58,39 @@ const quickJumpList = [
         navId: "data-tables",
     },
     {
+        label: "Step Navigation",
+        navId: "step-navigation",
+    },
+    {
         label: "Basic Forms",
         navId: "basic-forms",
     },
     {
         label: "Advanced Forms",
         navId: "advanced-forms",
+    },
+];
+
+const stepList = [
+    {
+        id: 1,
+        name: "Install Docker",
+        icon: "square-binary",
+    },
+    {
+        id: 2,
+        name: "Download Tech Bench ",
+        icon: "download",
+    },
+    {
+        id: 3,
+        name: "Run Docker Compose",
+        icon: "person-running",
+    },
+    {
+        id: 4,
+        name: "Enjoy",
+        icon: "face-smile",
     },
 ];
 </script>
