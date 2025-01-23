39ce605fa633ae60b38fdc0aabfd93b955edbd48
butcherman <ronbutcherii@gmail.com>
2025-01-22 19:27:03 -0800

    Change button and badge variant to computed property.

--- src/resources/js/Components/_Base/Buttons/BaseButton.vue
+++ src/resources/js/Components/_Base/Buttons/BaseButton.vue
@@ -19,7 +19,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { handleLinkClick } from "@/Composables/links.module";
-import { getVariantClass } from "@/Composables/styleData.module";
 
 const props = defineProps<{
     flat?: boolean;
@@ -74,5 +73,30 @@ const sizeClass = computed<string>(() => {
 | Background Color
 |-------------------------------------------------------------------------------
 */
-const variantClass = computed<string>(() => getVariantClass(props.variant));
+const variantClass = computed<string>(() => {
+    switch (props.variant) {
+        case "danger":
+            return "bg-rose-600 text-white";
+        case "dark":
+            return "bg-gray-900 text-white";
+        case "error":
+            return "bg-red-500 text-white";
+        case "help":
+            return "bg-violet-600 text-white";
+        case "info":
+            return "bg-blue-400 text-white";
+        case "light":
+            return "bg-neutral-300";
+        case "primary":
+            return "bg-blue-500 text-white";
+        case "secondary":
+            return "bg-blue-300";
+        case "success":
+            return "bg-green-500 text-white";
+        case "warning":
+            return "bg-yellow-400";
+        default:
+            return "bg-blue-500 text-white";
+    }
+});
 </script>
