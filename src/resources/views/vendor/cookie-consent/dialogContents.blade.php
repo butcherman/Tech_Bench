2f0ade39ba5542cdf3f016af50fae16e21dd7b83
butcherman <ronbutcherii@gmail.com>
2025-02-21 19:30:18 -0800

    Style Cookie Consent

---
+++ src/resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -0,0 +1,11 @@
+<div
+    class="js-cookie-consent absolute bottom-5 w-full min-h-12 bg-yellow-200 rounded-lg"
+    style="z-index: 10000"
+>
+    <div class="text-center h-full p-3">
+        {!! __('cookie-consent::texts.message') !!}
+        <button class="js-cookie-consent-agree cookie-consent__agree bg-blue-500 p-3 rounded-lg text-white">
+            {{ __('cookie-consent::texts.agree') }}
+        </button>
+    </div>
+</div>
