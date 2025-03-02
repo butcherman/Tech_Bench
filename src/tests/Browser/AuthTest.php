a1341c11becf20b3a89b541dcac0ed191705f7fe
butcherman <ronbutcherii@gmail.com>
2025-03-02 13:08:52 -0800

    Setup Laravel Dusk

---
+++ src/tests/Browser/AuthTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class AuthTest extends DuskTestCase
+{
+    public function test_base_home_page(): void
+    {
+        config(['services.azure.allow_login' => false]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('Tech Login')
+                ->assertSee('Forgot Password')
+                ->assertSee('Login');
+        });
+    }
+}
---
+++ src/tests/DuskTestCase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Process;
+use Laravel\Dusk\TestCase as BaseTestCase;
+use PHPUnit\Framework\Attributes\BeforeClass;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use DatabaseTruncation;
+
+    /**
+     * Prepare for Dusk test execution.
+     */
+    #[BeforeClass]
+    public static function prepare(): void
+    {
+        // if (! static::runningInSail()) {
+        //     static::startChromeDriver(['--port=9515']);
+        // }
+
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     */
+    protected function driver(): RemoteWebDriver
+    {
+        // Build Vite files to ensure they are up to date
+        Process::path('/app')->run('npm run build');
+
+        // Remove hot file
+        Process::path('/app/public')->run('rm hot');
+
+        $this->withoutVite();
+
+        $server = 'http://selenium:4444';
+
+        $options = (new ChromeOptions)->addArguments([
+            '--window-size=1920,1080',
+            '--whitelisted-ips=""',
+            '--ignore-certificate-errors',
+        ]);
+
+        $capabilities = DesiredCapabilities::chrome()
+            ->setCapability(ChromeOptions::CAPABILITY, $options);
+
+        return RemoteWebDriver::create(
+            $server,
+            $capabilities,
+        );
+    }
+}
