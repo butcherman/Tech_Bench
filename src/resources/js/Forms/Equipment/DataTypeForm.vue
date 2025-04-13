fa6d2106aafff490a4a5bd7941d23d397360f70e
butcherman <ronbutcherii@gmail.com>
2025-04-13 10:10:42 -0700

    Add update equipment data types.

--- src/resources/js/Forms/Equipment/DataTypeForm.vue
+++ src/resources/js/Forms/Equipment/DataTypeForm.vue
@@ -1,28 +1,101 @@
 <script setup lang="ts">
+import SwitchInput from "../_Base/SwitchInput.vue";
 import TextInput from "@/Forms/_Base/TextInput.vue";
 import VueForm from "@/Forms/_Base/VueForm.vue";
-import { object, string } from "yup";
 import { computed } from "vue";
+import { boolean, object, string } from "yup";
 
 const props = defineProps<{
-    edit?: any;
+    dataFieldType?: dataTypes;
 }>();
 
-const submitRoute = computed(() => (props.edit ? "#" : "#"));
-const submitMethod = computed(() => (props.edit ? "put" : "post"));
-const submitText = computed(() => (props.edit ? "Update" : "Submit"));
-const initValues = {};
-const schema = object({});
+/*
+|-------------------------------------------------------------------------------
+| Handle Form
+|-------------------------------------------------------------------------------
+*/
+const submitRoute = computed(() =>
+    props.dataFieldType
+        ? route("equipment-data.update", props.dataFieldType.type_id)
+        : route("equipment-data.store")
+);
+const submitMethod = computed(() => (props.dataFieldType ? "put" : "post"));
+const submitText = computed(() =>
+    props.dataFieldType ? "Update Data Type" : "Create Data Type"
+);
+
+/*
+|-------------------------------------------------------------------------------
+| Validation
+|-------------------------------------------------------------------------------
+*/
+const initValues = {
+    name: props.dataFieldType?.name || null,
+    pattern: props.dataFieldType?.pattern || null,
+    pattern_error: props.dataFieldType?.pattern_error || null,
+    masked: props.dataFieldType?.masked || false,
+    is_hyperlink: props.dataFieldType?.is_hyperlink || false,
+    allow_copy: props.dataFieldType?.allow_copy || false,
+    do_not_log_value: props.dataFieldType?.do_not_log_value || false,
+};
+
+const schema = object({
+    name: string().required(),
+    pattern: string().nullable(),
+    pattern_error: string().when("pattern", {
+        is: (val: string) => (val && val.length > 0 ? true : false),
+        then: (schema) =>
+            schema.required(
+                "Please enter an error message to tell the user about the needed pattern"
+            ),
+        otherwise: (schema) => schema.nullable(),
+    }),
+    masked: boolean().required(),
+    is_hyperlink: boolean().required(),
+    allow_copy: boolean().required(),
+    do_not_log_value: boolean().required(),
+});
 </script>
 
 <template>
     <VueForm
         :initial-values="initValues"
-        :submit-method="submitMethod"
-        :submit-route="submitRoute"
-        :submit-text="submitText"
         :validation-schema="schema"
+        :submit-route="submitRoute"
+        :submit-method="submitMethod"
+        :submit-text="submitText"
     >
-        <TextInput id="input" name="input" label="Input" focus />
+        <TextInput id="name" name="name" label="Name" />
+        <TextInput id="pattern" name="pattern" label="REGEX Pattern">
+            <template #start-group-text>
+                <div class="input-group-text">/</div>
+            </template>
+            <template #end-group-text>
+                <div class="input-group-text">/ g</div>
+            </template>
+        </TextInput>
+        <TextInput
+            id="error-msg"
+            name="pattern_error"
+            label="Error Message when Pattern is not Matched"
+        />
+        <div>
+            <SwitchInput id="masked" name="masked" label="Field is Masked" />
+            <SwitchInput
+                id="is-hyperlink"
+                name="is_hyperlink"
+                label="Field is A Hyperlink"
+            />
+            <SwitchInput
+                id="allow-copy"
+                name="allow_copy"
+                label="Add A Copy to Clipboard Button"
+            />
+            <SwitchInput
+                id="do-not-log-value"
+                name="do_not_log_value"
+                label="Do Not Log Field Value"
+            />
+        </div>
     </VueForm>
 </template>
---
+++ src/resources/js/Pages/Equipment/DataType/Create.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AppLayout from "@/Layouts/App/AppLayout.vue";
+import Card from "@/Components/_Base/Card.vue";
+import DataTypeForm from "@/Forms/Equipment/DataTypeForm.vue";
+import DataTypeHelp from "@/Components/Equipment/DataTypeHelp.vue";
+</script>
+
+<script lang="ts">
+export default { layout: AppLayout };
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-center">
+            <Card class="tb-card">
+                <DataTypeForm />
+            </Card>
+        </div>
+        <div class="flex justify-center">
+            <DataTypeHelp class="tb-card" />
+        </div>
+    </div>
+</template>
