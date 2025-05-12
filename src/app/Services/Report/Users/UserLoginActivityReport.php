5416335c7b2bcab09403c1ac49e36cc7f0bc7982
butcherman <ronbutcherii@gmail.com>
2025-05-11 17:08:54 -0700

    Add User Login Activity Report

--- src/app/Services/Report/Users/UserLoginActivityReport.php
+++ src/app/Services/Report/Users/UserLoginActivityReport.php
@@ -2,13 +2,53 @@
 
 namespace App\Services\Report\Users;
 
-class UserLoginActivityReport
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+class UserLoginActivityReport extends UserReportBase
 {
     /**
      * Create a new class instance.
      */
     public function __construct()
     {
-        //
+        $this->reportParamForm = 'UserLoginActivityReportForm';
+        $this->reportDataPage = 'UserLoginActivityReport';
+        $this->reportParamProps = ['user-list' => User::all()];
+    }
+
+    /**
+     * Validate the request to run the report.
+     */
+    public function getValidationParams(): array
+    {
+        return [
+            'start_date' => ['required', 'string'],
+            'end_date' => ['required', 'string'],
+            'user_list' => ['required', 'array'],
+        ];
+    }
+
+    /**
+     * Run the report
+     */
+    public function generateReportData(Collection $reportParams): array
+    {
+        $data = [];
+        $start = $reportParams->get('start_date');
+        $end = $reportParams->get('end_date');
+
+        foreach ($reportParams->get('user_list') as $username) {
+            $user = User::whereUsername($username)->first();
+
+            $loginData = $user->UserLogins()->whereBetween('created_at', [
+                $start,
+                $end
+            ])->get();
+
+            $data[$user->full_name] = $loginData->toArray();
+        }
+
+        return $data;
     }
 }
--- src/resources/js/Forms/Report/Users/UserLoginActivityReportForm.vue
+++ src/resources/js/Forms/Report/Users/UserLoginActivityReportForm.vue
@@ -1,28 +1,60 @@
 <script setup lang="ts">
-import TextInput from "@/Forms/_Base/TextInput.vue";
+import DatePicker from "@/Forms/_Base/DatePicker.vue";
+import PickListInput from "@/Forms/_Base/PickListInput.vue";
 import VueForm from "@/Forms/_Base/VueForm.vue";
-import { object, string } from "yup";
-import { computed } from "vue";
+import { object, date, array, ref } from "yup";
 
-const props = defineProps<{
-    edit?: any;
+defineProps<{
+    userList: user[];
 }>();
 
-const submitRoute = computed(() => (props.edit ? "#" : "#"));
-const submitMethod = computed(() => (props.edit ? "put" : "post"));
-const submitText = computed(() => (props.edit ? "Update" : "Submit"));
-const initValues = {};
-const schema = object({});
+const today = new Date();
+
+/*
+|-------------------------------------------------------------------------------
+| Vee Validate
+|-------------------------------------------------------------------------------
+*/
+const initValues = {
+    start_date: new Date(new Date().setDate(today.getDate() - 30)),
+    end_date: today,
+    user_list: [],
+};
+const schema = object({
+    start_date: date().required().label("Start Date"),
+    end_date: date()
+        .required()
+        .min(ref("start_date"), "Start Date must be after End Date")
+        .label("End Date"),
+    user_list: array().min(1).label("Users"),
+});
 </script>
 
 <template>
     <VueForm
+        submit-method="put"
+        submit-text="Run Report"
         :initial-values="initValues"
-        :submit-method="submitMethod"
-        :submit-route="submitRoute"
-        :submit-text="submitText"
+        :submit-route="
+            $route('reports.run', ['users', 'user-login-activity-report'])
+        "
         :validation-schema="schema"
     >
-        <TextInput id="input" name="input" label="Input" focus />
+        <div class="grid grid-cols-2 gap-3">
+            <DatePicker
+                id="start-date"
+                name="start_date"
+                label="Start Date"
+                focus
+            />
+            <DatePicker id="start-date" name="end_date" label="End Date" />
+        </div>
+        <PickListInput
+            id="user-list"
+            name="user_list"
+            :list="userList"
+            label-field="full_name"
+            value-field="username"
+        />
     </VueForm>
 </template>
