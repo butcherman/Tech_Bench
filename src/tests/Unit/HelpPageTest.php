84f9fb1b23b34a43ec370d0f7ea6221f54787775
butcherman <ronbutcherii@gmail.com>
2025-06-10 19:49:42 -0700

    Create Help Page Test

---
+++ src/tests/Unit/HelpPageTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class HelpPageTest extends TestCase
+{
+    /**
+     * These routes should not have a help page.
+     *
+     * @var array<int, string>
+     */
+    protected $bypassRoutes = [
+        'init.*',
+        'debugbar.*',
+        'dusk.*',
+        'login',
+        'password.*',
+        'two-factor.*',
+        'horizon.*',
+        'telescope',
+        'home',
+        'azure*',
+        'guest-link.*',
+        'download',
+        'publicTips.*',
+        'initialize',
+        'storage.local',
+        'admin.test-email',
+        '*.restore',
+
+        // TMP Removals
+        'customers.*',
+        'equipment.*',
+        'equipment-data.*',
+        'links.*',
+        'maint.*',
+        'user.*',
+        'reports.*',
+        'tech-tips.*',
+
+        'admin.tech-tips.*',
+        'admin.links.*',
+        'admin.logo.*',
+        'admin.features.edit',
+        'admin.security.*',
+        'admin.file-types.*',
+        'admin.phone-types.*',
+    ];
+
+    /*
+    |---------------------------------------------------------------------------
+    | Test to verify that all GET routes have a Help Page available.
+    |---------------------------------------------------------------------------
+    */
+    public function test_help_page_exists(): void
+    {
+        $routeList = collect(Route::getRoutes())->filter(function ($route) {
+            return in_array('GET', $route->methods);
+        })->map(function ($route) {
+            return $route->action;
+        })->pluck('as')->filter(function ($name) {
+            // return !is_null($name);
+            if (is_null($name)) {
+                return false;
+            }
+
+            foreach ($this->bypassRoutes as $bypass) {
+                if (Str::is($bypass, $name)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        foreach ($routeList as $route) {
+            $path = str_replace('.', DIRECTORY_SEPARATOR, $route);
+
+            $this->assertFileExists(
+                resource_path('js/Help/Routes/' . $path . '.vue')
+            );
+        }
+    }
+}
