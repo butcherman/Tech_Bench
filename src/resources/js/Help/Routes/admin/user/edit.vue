20065bebd760999f0e8885aecce0d4d9bd9546bf
butcherman <ronbutcherii@gmail.com>
2025-06-10 19:15:25 -0700

    Add User Administration Help pages

---
+++ src/resources/js/Help/Routes/admin/user/edit.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import UserForm from "@/Help/Forms/User/UserForm.vue";
+
+const title = "Help";
+
+const getTitle = () => {
+    return title;
+};
+
+defineExpose({ getTitle });
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <p>Modify a User Profile by entering the following information.</p>
+        <UserForm />
+    </div>
+</template>
---
+++ src/resources/js/Help/Routes/admin/user/show.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import TableStacked from "@/Components/_Base/TableStacked.vue";
+
+const title = "Help";
+
+const userActions = {
+    reset_users_password: `Manually enter a new password for the user.`,
+    send_reset_password_link: `This will create a link for the user to reset
+        their own password.`,
+    update_user_information: `Update User Profile information`,
+    disable_user: `Immediately deactivate the user. Profile will not be deleted,
+        but the user will not be able to access their account.`,
+};
+
+const getTitle = () => {
+    return title;
+};
+
+defineExpose({ getTitle });
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <p>
+            Specific details about the user are shown here. Use the Actions
+            buttons to make changes to the users profile.
+        </p>
+        <TableStacked :items="userActions" />
+    </div>
+</template>
