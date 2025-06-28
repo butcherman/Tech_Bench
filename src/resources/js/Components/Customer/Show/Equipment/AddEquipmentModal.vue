6c956deddaa85bbab6369f61b7e53268e08cfa9c
butcherman <ronbutcherii@gmail.com>
2025-06-27 20:15:56 -0700

    Add Current Site to default when adding new equipment to customer

--- src/resources/js/Components/Customer/Show/Equipment/AddEquipmentModal.vue
+++ src/resources/js/Components/Customer/Show/Equipment/AddEquipmentModal.vue
@@ -2,7 +2,11 @@
 import Modal from "@/Components/_Base/Modal.vue";
 import NewCustomerEquipmentForm from "@/Forms/Customer/NewCustomerEquipmentForm.vue";
 import { availableEquipment } from "@/Composables/Equipment/EquipmentData.module";
-import { customer, siteList } from "@/Composables/Customer/CustomerData.module";
+import {
+    currentSite,
+    customer,
+    siteList,
+} from "@/Composables/Customer/CustomerData.module";
 import { useTemplateRef } from "vue";
 
 const modal = useTemplateRef("add-equipment-modal");
@@ -25,6 +29,7 @@ defineExpose({ show });
             :available-equipment-list="availableEquipment"
             :customer="customer"
             :site-list="siteList ?? []"
+            :current-site="currentSite"
             @success="modal?.hide"
         />
     </Modal>
