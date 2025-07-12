d5de3045ed67af458883f2802eb12f8b04ac0b89
butcherman <ronbutcherii@gmail.com>
2025-07-11 20:49:51 -0700

    Add Range Input

--- src/resources/js/Composables/Equipment/WorkbookElements.ts
+++ src/resources/js/Composables/Equipment/WorkbookElements.ts
@@ -301,4 +301,49 @@ export const elementList = computed<workbookElement[]>(() => [
             buttonIcon: "phone",
         },
     },
+    {
+        index: "13",
+        type: "input",
+        tag: "input",
+        component: "RangeInput",
+        props: {
+            label: "",
+            valueText: "",
+            min: 0,
+            max: 100,
+            help: "",
+        },
+        assist: {
+            label: {
+                label: "Label",
+                help: "Label for Input Text",
+                type: "string",
+            },
+            valueText: {
+                label: "Value Text",
+                help: "Text to show next to the selected value",
+                type: "string",
+            },
+            min: {
+                label: "Minimum Value",
+                help: "Lowest Number in the Range",
+                type: "number",
+            },
+            max: {
+                label: "Maximum Value",
+                help: "Highest Number in the Range",
+                type: "number",
+            },
+            help: {
+                label: "Help Text",
+                help: "Helpful text to show when Input is active",
+                type: "string",
+            },
+        },
+        componentData: {
+            label: "Range Input",
+            help: "Slider to Pick A Number",
+            buttonIcon: "sliders",
+        },
+    },
 ]);
