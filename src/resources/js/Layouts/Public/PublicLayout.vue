0e573db949426ad3e06ae34d5869bea6d9c72eae
butcherman <ronbutcherii@gmail.com>
2025-07-20 10:34:27 -0700

    Add Public Layout

---
+++ src/resources/js/Layouts/Public/PublicLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppFooter from "../App/AppFooter.vue";
+import PublicHeader from "./PublicHeader.vue";
+</script>
+
+<template>
+    <div id="kb-layout-wrapper" class="flex flex-col bg-gray-200 h-screen">
+        <PublicHeader />
+        <section class="grow mt-14 p-3">
+            <slot />
+        </section>
+        <AppFooter class="bg-slate-100" />
+    </div>
+</template>
