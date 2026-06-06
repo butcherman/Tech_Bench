ef94b0604b8e9b077baf16b4052540ed2b956494
butcherman <ronbutcherii@gmail.com>
2026-06-06 20:05:42 +0000

    Move workbook show header to its own component

--- src/resources/js/Pages/Customer/Workbook/Index.vue
+++ src/resources/js/Pages/Customer/Workbook/Index.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import AtomLoader from "@/Components/_Base/Loaders/AtomLoader.vue";
-import BaseButton from "@/Components/_Base/Buttons/BaseButton.vue";
 import Card from "@/Components/_Base/Card.vue";
-import ClipboardCopy from "@/Components/_Base/ClipboardCopy.vue";
 import PublicLayout from "@/Layouts/Public/PublicLayout.vue";
-import PublishWorkbook from "@/Components/Workbook/PublishWorkbook.vue";
+import ShowHeader from "@/Components/Workbook/ShowHeader.vue";
 import WorkbookWrapper from "@/Components/Workbook/WorkbookWrapper.vue";
 import { initWorkbook } from "@/Composables/Workbook/CustomerWorkbook.module";
 import { onMounted } from "vue";
@@ -30,55 +28,11 @@ export default { layout: PublicLayout };
 
 <template>
     <div class="flex flex-col h-full">
-        <div class="mb-2 pb-2 flex border-b border-slate-300">
-            <div>
-                <BaseButton
-                    icon="arrow-left"
-                    text="Back to Equipment"
-                    :href="
-                        $route('customers.equipment.show', [
-                            customer.slug,
-                            equipment.cust_equip_id,
-                        ])
-                    "
-                />
-            </div>
-            <div class="text-center grow">
-                <div v-if="workbook.published">
-                    <p>Public Workbook Link</p>
-                    <p>
-                        <a
-                            :href="
-                                $route(
-                                    'cust-workbook.show',
-                                    props.workbook.wb_hash,
-                                )
-                            "
-                            target="_blank"
-                            class="text-blue-600 me-2"
-                        >
-                            {{
-                                $route(
-                                    "cust-workbook.show",
-                                    props.workbook.wb_hash,
-                                )
-                            }}
-                        </a>
-                        <ClipboardCopy
-                            :value="
-                                $route(
-                                    'cust-workbook.show',
-                                    props.workbook?.wb_hash,
-                                )
-                            "
-                        />
-                    </p>
-                </div>
-            </div>
-            <div>
-                <PublishWorkbook :equipment="equipment" :workbook="workbook" />
-            </div>
-        </div>
+        <ShowHeader
+            :customer="customer"
+            :equipment="equipment"
+            :workbook="workbook"
+        />
         <div class="grow">
             <Deferred data="workbook-values">
                 <template #fallback>
