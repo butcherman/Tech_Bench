00ca7f5f0e1d92f2612f710a23bf34ad8c64893b
butcherman <ronbutcherii@gmail.com>
2026-07-18 17:49:35 +0000

    Updated testing

--- src/database/factories/WorkbookTaskListFactory.php
+++ src/database/factories/WorkbookTaskListFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\CustomerEquipmentWorkbook;
 use App\Models\WorkbookTaskList;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<WorkbookTaskList>
@@ -11,14 +13,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class WorkbookTaskListFactory extends Factory
 {
     /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
+     * Define the model's default state
      */
     public function definition(): array
     {
         return [
-            //
+            'wb_id' => CustomerEquipmentWorkbook::factory(),
+            'list_index' => Str::uuid(),
+            'locked' => false,
+            'public' => true,
         ];
     }
 }
