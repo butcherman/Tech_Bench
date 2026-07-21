f67bddb05a061ef67d5aa00f10b8f7cdd46470b1
butcherman <ronbutcherii@gmail.com>
2026-07-21 15:32:46 +0000

    Add static alert

--- src/resources/js/layouts/components/StaticAlert.vue
+++ src/resources/js/layouts/components/StaticAlert.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-//
+import { usePage } from "@inertiajs/vue3";
+
+// TODO - Format error messages properly
+const { props } = usePage();
 </script>
 
 <template>
-    <h1>Static Alert</h1>
+    <div>
+        <div
+            v-for="err in props.errors"
+            class="bg-red-600 flex py-2 px-4 rounded-lg justify-between text-white"
+        >
+            <div><fa-icon icon="triangle-exclamation" /></div>
+            <div>{{ err }}</div>
+            <div><fa-icon icon="triangle-exclamation" /></div>
+        </div>
+    </div>
 </template>
