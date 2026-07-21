0ea21a3dd883da2914b60e0a106b5670609626bb
butcherman <ronbutcherii@gmail.com>
2026-07-21 03:52:46 +0000

    Add animation to settings menu

--- src/resources/js/features/user/components/UserSettingsMenu.vue
+++ src/resources/js/features/user/components/UserSettingsMenu.vue
@@ -36,33 +36,47 @@ const menuList = ref<menuItem[]>([
 </script>
 
 <template>
-    <div v-if="isOpen" class="min-w-50 rounded-lg px-4 py-2">
-        <h6
-            class="text-center border-b border-b-slate-200 text-muted pb-2 mb-2"
-        >
-            {{ user.full_name }}
-        </h6>
-        <ul>
-            <li
-                v-for="item in menuList"
-                :key="item.label"
-                class="py-2 px-2 hover:bg-slate-100 rounded-md"
+    <Transition name="settings-menu" appear>
+        <div v-if="isOpen" class="min-w-50 rounded-lg px-4 py-2">
+            <h6
+                class="text-center border-b border-b-slate-200 text-muted pb-2 mb-2"
             >
-                <Link :href="item.route">
-                    <fa-icon :icon="item.icon" />
-                    {{ item.label }}
-                </Link>
-            </li>
-            <li class="border-t border-t-slate-200">
-                <Link
-                    :href="logout.url()"
-                    method="POST"
-                    class="w-full block py-2 px-4 pointer text-start hover:bg-slate-100 rounded-md"
+                {{ user.full_name }}
+            </h6>
+            <ul>
+                <li
+                    v-for="item in menuList"
+                    :key="item.label"
+                    class="py-2 px-2 hover:bg-slate-100 rounded-md"
                 >
-                    <fa-icon icon="sign-out-alt" />
-                    Logout
-                </Link>
-            </li>
-        </ul>
-    </div>
+                    <Link :href="item.route">
+                        <fa-icon :icon="item.icon" />
+                        {{ item.label }}
+                    </Link>
+                </li>
+                <li class="border-t border-t-slate-200">
+                    <Link
+                        :href="logout.url()"
+                        method="POST"
+                        class="w-full block py-2 px-4 pointer text-start hover:bg-slate-100 rounded-md"
+                    >
+                        <fa-icon icon="sign-out-alt" />
+                        Logout
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </Transition>
 </template>
+
+<style scoped>
+.settings-menu-enter-active,
+.settings-menu-leave-active {
+    transition: opacity 250ms ease;
+}
+
+.settings-menu-enter-from,
+.settings-menu-leave-to {
+    opacity: 0;
+}
+</style>
