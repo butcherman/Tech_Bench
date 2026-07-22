28c513270b9bbed9c266242b5612b8fd69657259
butcherman <ronbutcherii@gmail.com>
2026-07-22 00:59:45 +0000

    Add Help Forms

---
+++ src/resources/js/help/forms/maintenance/LogSettingsForm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TableStacked from "@/core/components/TableStacked.vue";
+
+const tableData = {
+    days_to_keep_log_files: `Log files are automatically deleted after this
+        many days`,
+    logging_level: `Select the minimum level for logging.`,
+};
+</script>
+
+<template>
+    <TableStacked :items="tableData" />
+</template>
