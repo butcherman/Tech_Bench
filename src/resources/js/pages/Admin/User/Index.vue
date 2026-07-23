14387971d2e508d257579f4525e21689739ea1ff
butcherman <ronbutcherii@gmail.com>
2026-07-23 03:19:00 +0000

    Add User Administration Page

---
+++ src/resources/js/pages/Admin/User/Index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AddButton from "@/core/components/buttons/AddButton.vue";
+import AppLayout from "@/layouts/AppLayout.vue";
+import AtomLoader from "@/core/components/loaders/AtomLoader.vue";
+import Card from "@/core/components/Card.vue";
+import { Deferred } from "@inertiajs/vue3";
+
+defineProps<{
+    userList?: User[];
+}>();
+</script>
+
+<script lang="ts">
+export default { layout: AppLayout };
+</script>
+<template>
+    <div class="flex justify-center">
+        <Card title="User Admistration">
+            <template #append-title>
+                <AddButton text="New User" size="small" pill />
+            </template>
+            <Deferred data="user-list">
+                <template #fallback>
+                    <div class="flex justify-center">
+                        <AtomLoader text="Loading Users" />
+                    </div>
+                </template>
+            </Deferred>
+            user list
+        </Card>
+    </div>
+</template>
