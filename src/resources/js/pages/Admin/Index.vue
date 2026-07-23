c9e4de0f11836ece79e9996571a33d11d0ce17e9
butcherman <ronbutcherii@gmail.com>
2026-07-23 02:12:38 +0000

    Add Administration Home Page

---
+++ src/resources/js/pages/Admin/Index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppLayout from "@/layouts/AppLayout.vue";
+import Card from "@/core/components/Card.vue";
+import MenuList from "@/core/components/MenuList.vue";
+
+const props = defineProps<{
+    menu: {
+        [key: string]: menuItem[];
+    };
+}>();
+</script>
+
+<script lang="ts">
+export default { layout: AppLayout };
+</script>
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <template v-for="(items, group) in menu" :key="group">
+            <Card v-if="items.length" :title="group.toString()" size="large">
+                <MenuList :menu-list="items" />
+            </Card>
+        </template>
+    </div>
+</template>
