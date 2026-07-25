dd0d2958ba3f2e061be14df30bae367870330617
butcherman <ronbutcherii@gmail.com>
2026-07-25 20:58:09 +0000

    Add Disable User

--- src/resources/js/features/user/components/UserAdministration.vue
+++ src/resources/js/features/user/components/UserAdministration.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import BaseBadge from "@/core/components/badges/BaseBadge.vue";
 import DataTable from "@/features/dataTable/DataTable.vue";
+import verifyModal from "@/core/composables/verifyModal";
+import { destroy } from "@/wayfinder/routes/admin/user";
+import { router } from "@inertiajs/vue3";
 import { show } from "@/wayfinder/routes/admin/user";
 import { useLinkHelper } from "@/core/composables/linkHelper";
 import { useUserAdministrationHelper } from "../composables/userAdministrationHelper";
+import { useUserAuth } from "@/core/state/userAuth";
 
 defineProps<{
     userList: User[];
 }>();
 
 const { userTableColumns } = useUserAdministrationHelper();
+const { user } = useUserAuth();
 
 /**
  * When a row is clicked on, go to that users profile
@@ -27,6 +32,21 @@ const rowLink = (event: MouseEvent, userRow: User): void => {
  */
 const disableUser = (userRow: User): void => {
     console.log(userRow);
+
+    if (user && userRow.username === user.value?.username) {
+        alert("You cannot disable yourself");
+        return;
+    }
+
+    verifyModal("This User will no longer be allowed access").then((res) => {
+        if (res) {
+            console.log("go for it");
+            router.delete(destroy.url(userRow.username), {
+                preserveScroll: true,
+                async: true,
+            });
+        }
+    });
 };
 </script>
 
