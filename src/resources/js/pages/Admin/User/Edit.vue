f30b5d88613ad78a01b1c7672d57b9d5d2d19954
butcherman <ronbutcherii@gmail.com>
2026-07-27 21:05:39 +0000

    Add Edit User Page

---
+++ src/resources/js/pages/Admin/User/Edit.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from "@/layouts/AppLayout.vue";
+import Card from "@/core/components/Card.vue";
+import UserDetailsForm from "@/features/user/forms/UserDetailsForm.vue";
+
+defineProps<{
+    roles: UserRole[];
+    user: User;
+}>();
+</script>
+
+<script lang="ts">
+export default { layout: AppLayout };
+</script>
+<template>
+    <div class="flex justify-center">
+        <Card title="New User">
+            <UserDetailsForm :roles="roles" :user="user" />
+        </Card>
+    </div>
+</template>
