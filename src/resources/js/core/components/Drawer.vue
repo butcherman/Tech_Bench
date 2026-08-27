10762adb4ae7e267c09e54cb076510db83585027
butcherman <ronbutcherii@gmail.com>
2026-08-27 03:25:46 +0000

    Update core components to use defineModel

--- src/resources/js/core/components/Drawer.vue
+++ src/resources/js/core/components/Drawer.vue
@@ -2,13 +2,15 @@
 import BaseBadge from "./badges/BaseBadge.vue";
 import { computed } from "vue";
 
-const emit = defineEmits(["update:show"]);
-
 const props = defineProps<{
-    show: boolean;
     position?: Placement;
     title?: string;
 }>();
+
+const show = defineModel({
+    required: true,
+    default: false,
+});
 
 const positionClass = computed(() => {
     switch (props.position) {
@@ -22,29 +24,21 @@ const positionClass = computed(() => {
             return "bottom-0 left-0 right-0 border-t w-full min-h-96";
     }
 });
-
-/**
- * Status of the Drawer - opened or closed
- */
-const isOpen = computed({
-    get: () => props.show,
-    set: (value) => emit("update:show", value),
-});
 </script>
 
 <template>
     <Teleport to="body">
-        <div v-if="isOpen" id="drawer-wrapper">
+        <div v-if="show" id="drawer-wrapper">
             <Transition name="drawer-backdrop" appear>
                 <div
-                    v-if="isOpen"
+                    v-if="show"
                     class="fixed inset-0 bg-gray-500/75 z-40"
-                    @click="isOpen = false"
+                    @click="show = false"
                 />
             </Transition>
             <Transition :name="`drawer-${position ?? 'bottom'}`" appear>
                 <div
-                    v-if="isOpen"
+                    v-if="show"
                     id="drawer"
                     class="fixed p-4 overflow-y-auto bg-white border-slate-300 z-50"
                     :class="positionClass"
@@ -58,7 +52,7 @@ const isOpen = computed({
                             variant="light"
                             class="text-white pointer"
                             circle
-                            @click="isOpen = false"
+                            @click="show = false"
                         />
                         <h5 class="grow text-muted">
                             {{ title }}
